866e36730cb66213ba606fe6a9eae2d85abae7f1
Jesse MacFadyen <purplecabbage@gmail.com>
2014-01-15 00:31:19 -0800

    stubbed stuff

---
+++ src/wp/CordovaViewControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using Microsoft.Phone.Tasks;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+
+    public class CordovaViewControl : BaseCommand
+    {
+
+        public void loadView(String optionStr)
+        {
+
+        }
+    }
+}
---
+++ www/CordovaViewControl.js
@@ -0,0 +1,12 @@
+﻿
+// (c) 2011-2014 Jesse MacFadyen,  Adobe Systems Incorporated
+
+var exec = require('cordova/exec');
+
+module.exports = {
+
+	loadView:function(strPath) {
+		exec(null,null,"CordovaViewControl","loadView",[strPath]);
+	}
+
+}
