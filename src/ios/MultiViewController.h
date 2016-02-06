c3e75e1249b20dc2e19dab0285caea11c8bb6517
Jesse MacFadyen <purplecabbage@gmail.com>
2016-02-06 00:22:07 -0800

    more ios

---
+++ src/ios/MultiViewController.h
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface RJMultiViewController : CDVPlugin
+{}
+
+
+- (void)loadView:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/MultiViewController.m
@@ -0,0 +1,20 @@
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include "TargetConditionals.h"
+
+#import <Cordova/CDV.h>
+#import "MultiViewController.h"
+
+
+@implementation RJMultiViewController
+
+
+
+- (void)loadView:(CDVInvokedUrlCommand*)command
+{
+   // command.callbackId
+    //[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
